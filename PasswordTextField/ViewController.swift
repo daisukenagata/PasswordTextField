73341280fc1753fe4e0aec625682d6e2cfd5dd92
nagatadaisuke <dbank0208@gmail.com>
2017-05-27 22:17:50 +0900

    add

--- PasswordTextField/ViewController.swift
+++ PasswordTextField/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController,UITextFieldDelegate {
         //MARK: - UITextFieldDelegate
         
         self.textField.delegate = self
-        
+        textField.addSubview(KeyboardView())
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -39,4 +39,42 @@ class ViewController: UIViewController,UITextFieldDelegate {
         return true
     }
 
+}
+
+protocol KeyboardDelegate: class {
+    func keyWasTapped(text: String)
+}
+
+class KeyboardView: UIView {
+    
+    // This variable will be set as the view controller so that
+    // the keyboard can send messages to the view controller.
+    weak var delegate: KeyboardDelegate?
+    
+    
+    // MARK:- keyboard initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+       // initializeSubviews()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initializeSubviews()
+    }
+    
+    func initializeSubviews() {
+        let xibFileName = "KeyboardView" // xib extention not included
+        let view = Bundle.main.loadNibNamed(xibFileName, owner: self, options: nil)?[0] as! UIView
+        self.addSubview(view)
+        view.frame = self.bounds
+    }
+    
+    // MARK:- Button actions from .xib file
+    @IBAction func keyTapped(_ sender: UIButton) {
+        // When a button is tapped, send that information to the
+        // delegate (ie, the view controller)
+        self.delegate?.keyWasTapped(text: sender.titleLabel!.text!) // could alternatively send a tag value
+    }
+    
 }
